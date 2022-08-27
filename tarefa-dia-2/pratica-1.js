4f417dc5d3fd72de0c201a5763ab593a5a10fbb7
Muzer4 <Muryllo428@gmail.com>
2022-08-27 16:42:38 -0300

    pratica-1 modificado

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,13 @@
 let nome = "Muryllo Soares"
-let email = "Muryllo428@gmail.com"
-let profissao = "comprador"
-let idade = "26"
-let lugar = "japao"
-
 console.log(nome)
+let email = "Muryllo428@gmail.com"
 console.log(email)
+let profissao = "comprador"
 console.log(profissao)
+let idade = "26"
 console.log(idade)
+let lugar = "japao"
 console.log(lugar)
+
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem`)
