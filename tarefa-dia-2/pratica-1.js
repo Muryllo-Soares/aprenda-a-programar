f8014fc69f53cb6eff7b25fb8c85616fad7db1c0
Muzer4 <Muryllo428@gmail.com>
2022-08-27 16:01:01 -0300

    pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "Muryllo428@gmail.com"
 let profissao = "comprador"
 let idade = "26"
 let lugar = "japao"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
